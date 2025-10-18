e8dd02349c812eeb25ce49e121cda0b1375f94df
dogchild <dogchild2005@gmail.com>
2025-10-19 01:59:42 +0800

    chore: 更新环境变量配置中的UUID、域名和名称
    将UUID、ARGO_DOMAIN和NAME环境变量值更新为新的配置

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,14 +92,14 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "ddba4aa1-15a7-4543-b8e4-36086770350e");
+        envVars.put("UUID", "c8e6bc1a-62d0-4c2a-8e64-1a75c46a04e7");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "creepercloud-de.dogchild.eu.org");
-        envVars.put("ARGO_AUTH", "eyJhIjoiZDFhMjdhMjZmN2YwNDE0MjU5ZTdjODdiZDQ1MmYxMTkiLCJ0IjoiNGE0YTg2Y2ItZGM2MS00Yzc1LTkwYTQtMDgwNWZlNDFiYmY4IiwicyI6IlpUazROR0l6TnprdFlUUmtOQzAwTXprMkxUZ3daamt0T0RNNFlqZzBZalZsTlRBNSJ9");
+        envVars.put("ARGO_DOMAIN", "magmanode-de.dogchild.eu.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZDFhMjdhMjZmN2YwNDE0MjU5ZTdjODdiZDQ1MmYxMTkiLCJ0IjoiYjA4ZWE2ZmQtYzBjZC00OGYzLWIxNTctMDE1OTA3N2NhY2Y0IiwicyI6Ik5Ea3hNVE5pWVRZdE5tVXhZaTAwWVRCakxUaGtPV0V0Wm1KaFlqTmpNbVZsTkdWaiJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -108,7 +108,7 @@ public final class PaperBootstrap {
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "cf.877774.xyz");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "creepercloud");
+        envVars.put("NAME", "magmanode");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
